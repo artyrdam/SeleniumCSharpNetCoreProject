34883a1b79750c3778a47278e8de1f75e97f845d
artyrdam <artydam@live.co.uk>
2021-12-13 19:19:55 +0000

    Made changes to headless

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -24,7 +24,7 @@ namespace TestProject1
             ChromeOptions opt = new ChromeOptions();
             opt.AddArguments("headless");
             //headless11
-            driver = new ChromeDriver(opt);
+            driver = new ChromeDriver();
         }
 
         
